5da1da5a41d820edfc5d3334b17c6d108f87600b
Markus Auvo <markus.auvo@helsinki.fi>
2019-01-06 21:48:05 +0200

    ...

--- config/environment.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# korvaa tämä omalla käyttäjätunnuksellasi
-USERNAME="kalleilv"
-# korvaa tämä haluamallasi kansion nimellä
-PROJECT_FOLDER="tsoha"
-# sovelluksesi tulee sijaitsemaan osoitteessa USERNAME.users.cs.helsinki.fi/PROJECT_FOLDER
